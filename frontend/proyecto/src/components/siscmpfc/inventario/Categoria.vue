69ca8510ad7a5d40587435bfbd35f7b6aebdbbe6
Cristian Laguna <cristiandavidlaguna.97@gmail.com>
2021-06-30 21:36:32 -0500

    modal de categoria

--- frontend/proyecto/src/components/siscmpfc/inventario/Categoria.vue
+++ frontend/proyecto/src/components/siscmpfc/inventario/Categoria.vue
@@ -1,26 +1,36 @@
 <template>
   <div>
-    <v-row>
-      <v-col>
-        <v-text-field
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Buscar"
-        ></v-text-field>
-        <v-data-table
-          :headers="headers"
-          :items="items"
-          class="elevation-1"
-          :loading="loading"
-          loading-text='Cargando...'
-          :search="search"
-        >
-          <template v-slot:no-data>
-            <v-btn color="primary" @click="iniciar">Reiniciar</v-btn>
-          </template>
-        </v-data-table>
-      </v-col>
-    </v-row>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-data-table
+            :headers="headers"
+            :items="items"
+            class="elevation-1"
+            :loading="loading"
+            loading-text='Cargando...'
+            :search="search"
+          >
+            <template v-slot:top>
+              <v-toolbar flat color="white">
+                <v-toolbar-title>Categoría</v-toolbar-title>
+                <v-divider class="mx-4" inset vertical></v-divider>
+                <v-text-field
+                  v-model="search"
+                  append-icon="mdi-magnify"
+                  label="Buscar"
+                ></v-text-field>
+                <v-spacer></v-spacer>
+                <v-btn color="warning" dark class="mb-2" @click="iniciar"><v-icon>mdi-cached</v-icon></v-btn>
+              </v-toolbar>
+            </template>
+            <template v-slot:no-datañ>
+              <v-btn color="primary" @click="iniciar">Reiniciar</v-btn>
+            </template>
+          </v-data-table>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 <script lang="ts">
@@ -37,15 +47,23 @@ export default class Categoria extends Vue {
 
   items:Array<any> = []
   api:any = new ApiInv
+  loading:boolean = false
   search:string = ''
   headers:Array<any> = [
     {text: 'ID', value:'id'},
-    {text: 'Descripcion', value: 'descripcion'}
+    {text: 'Descripcion', sortable: true, value: 'descripcion'}
   ]
 
   async iniciar(){
-    let r = await this.api.getCategorias()
-    this.items = r
+    try{
+      this.loading = true
+      let r = await this.api.getCategorias()
+      this.items = r
+    } catch(error){
+      alert('Error')
+    } finally{
+      this.loading = false
+    }
   }
   //se ejecuta cuando se crea el componente
   created() {
---
+++ frontend/proyecto/src/components/siscmpfc/inventario/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-dialog :v-model="dialog" max-width="500px">
+      <template v-slot:activator="{on, attrs}">
+        <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on"><v-icon>mdi-add_box</v-icon></v-btn>
+      </template>
+        <v-card>
+          
+          <v-card-title primary-title>
+            <span class="headline">{{ formTitle }}</span>
+          </v-card-title>
+
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="2" sm="2" md="2">
+                  <v-text-field v-model="editedItem.id" ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+          
+          <v-card-actions>
+            <v-btn flat color="primary">text</v-btn>
+            <v-btn flat color="primary">text</v-btn>
+          </v-card-actions>
+        </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import {ApiInv} from './ApiInv';
+
+
+@Component({
+  components:{
+    
+  }
+})
+export default class Modal extends Vue {
+ 
+  dialog:boolean = true
+
+}
+</script>
